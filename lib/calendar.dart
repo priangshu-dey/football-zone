3fe896a60e0746cf08d9b0fd6b16d8b508d3d7ca
maruf908548 <94357135+maruf908548@users.noreply.github.com>
2022-01-26 17:30:01 +0600

    Update calendar.dart

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -1,7 +1,17 @@
+
+
+
+
 import 'package:flutter/material.dart';
 
-class CalendarTest extends StatelessWidget {
+class CalendarTest extends StatefulWidget {
   const CalendarTest({Key? key}) : super(key: key);
+
+  @override
+  State<CalendarTest> createState() => _CalendarTestState();
+}
+
+class _CalendarTestState extends State<CalendarTest> {
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +19,20 @@ class CalendarTest extends StatelessWidget {
       appBar: AppBar(
         title: Text("test"),
       ),
-      body: Text("text"),
+      body: Column(
+        children: [
+          Text("text"),
+          ElevatedButton(onPressed: ()async{
+             final  DateTime?  Picked  = await showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(2022,1),
+                lastDate: DateTime(2022,2));
+          },
+              child: Text('Text')
+          ),
+      ]
+    )
     );
   }
-}
+ }
